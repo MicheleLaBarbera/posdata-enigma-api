c7117c9301f11d92a89772f5f0d1a8b3eb5c50be
Michele La Barbera <mlabarbera@POSDATA.net>
2018-10-26 00:50:47 +0200

    last changes

--- controllers/users.js
+++ controllers/users.js
@@ -283,7 +283,8 @@ module.exports = {
                 sites.push(customerSiteObject);    
             }       
         });
-        res.status(200).json(sortByKey(sites, 'name'));  
+//console.log(sites);
+        res.status(200).json(/*sites.sort(predicate('name'))*/sites);  
     },
     getUserCustomerSiteHostgroups: async (req, res, next) => {
         const userId = req.value.params.userId;
@@ -585,3 +586,59 @@ module.exports = {
         }
     }
 };
+
+
+function predicate() {
+    var fields = [], n_fields = arguments.length, field, name, reverse, cmp;
+
+    var default_cmp = function (a, b) {
+        if (a === b) return 0;
+        return a < b ? -1 : 1;
+        },
+        getCmpFunc = function (primer, reverse) {
+        var dfc = default_cmp,
+            // closer in scope
+            cmp = default_cmp;
+        if (primer) {
+            cmp = function (a, b) {
+            return dfc(primer(a), primer(b));
+            };
+        }
+        if (reverse) {
+            return function (a, b) {
+            return -1 * cmp(a, b);
+            };
+        }
+        return cmp;
+        };
+
+    // preprocess sorting options
+    for (var i = 0; i < n_fields; i++) {
+        field = arguments[i];
+        if (typeof field === 'string') {
+        name = field;
+        cmp = default_cmp;
+        } else {
+        name = field.name;
+        cmp = getCmpFunc(field.primer, field.reverse);
+        }
+        fields.push({
+        name: name,
+        cmp: cmp
+        });
+    }
+
+    // final comparison function
+    return function (A, B) {
+        var a, b, name, result;
+        for (var i = 0; i < n_fields; i++) {
+        result = 0;
+        field = fields[i];
+        name = field.name;
+
+        result = field.cmp(A[name], B[name]);
+        if (result !== 0) break;
+        }
+        return result;
+    };
+}
